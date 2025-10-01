9ff8c22f524338262ad9570f3de366286b850b1c
norviewhstech003 <norviewhstech@gmail.com>
2025-10-01 12:10:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const fallbackSubmit = document.getElementById("fallback-submit");
 
   // 👇 Paste your latest Web App URL here after each Apps Script deployment
-  const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzW2pwrG7lQ9cAuBFnEIns0S9k36Zi7IyAHUa5C5mHpbX8e-ayqCarf7MQj29vP1dZd/exec";
+  const WEB_APP_URL = "https://script.google.com/macros/s/AKfycby-FWzipayQL6tOP9xEeq3jbBKqlKMaLtndEcxcPA8ogNz7Msr8YUkbnlKLLB7aOBRK/exec";
 
   // Show today's date
   const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
